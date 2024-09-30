04c368577ee1c4b913c6a9d4785c8be516c7524f
VinayRoyal2208 <vinayroyals2208@gmail.com>
2024-09-30 15:28:01 +0530

    Update Login.java

--- Login.java
+++ Login.java
@@ -19,3 +19,19 @@ class Main {
     System.out.println("Sum is: " + result);
   }
 }
+
+#WEBHOOK JOB DEMO
+
+public class Main {
+
+  public static void main(String[] args) {
+    int rows = 5;
+
+    for (int i = 1; i <= rows; ++i) {
+      for (int j = 1; j <= i; ++j) {
+        System.out.print("* ");
+      }
+      System.out.println();
+    }
+  }
+}
